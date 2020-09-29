e45f69d1e024e1cbcaa7ea84578c22b3686bb82c
Nuradin Ibrahim <nuradinibrahim@Nuradins-MacBook-Air.local>
2020-09-29 08:54:17 -0600

    Added functionality to one button

---
+++ src/components/joke-button.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h1 id="api">{{ joke }}</h1>
+    <button @click="loudJoke" id="loud-button">Normal Joke</button>
+    <button id="snake-button">Snake Button</button>
+    <button id="normal-button">Loud Button</button>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+    data() {
+        return {
+            joke: "Normal Joke"
+        }
+    },
+  methods: {
+    loudJoke: function() {
+      axios
+        .request({
+          url: "https://geek-jokes.sameerkumar.website/api?format=json",
+          method: "GET"
+        })
+        .then(response => {
+          this.joke = response.data.joke;
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/joke.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'normalJoke',
+    
+    }
+</script>
+
+<style scoped>
+
+</style>
